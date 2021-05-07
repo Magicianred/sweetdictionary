fba8aeadf5ce8694f6bdb087218a1c2f3c8e6c30
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2021-05-07 14:22:03 +0300

    sweetdictionary

---
+++ SweetDictionary/SweetDictionary/Controllers/AdminCategoryController.cs
@@ -0,0 +1,47 @@
+﻿using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.EntityFramework;
+using Entity.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SweetDictionary.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        public ActionResult Index()
+        {
+            var categoryValues = categoryManager.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category c)
+        {
+            CategoryValidation categoryValidator = new CategoryValidation();
+            ValidationResult result = categoryValidator.Validate(c);
+            if(result.IsValid)
+            {
+                categoryManager.CategoryAdd(c);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
